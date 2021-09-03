adf3eca8f4b5df80c9a435d5c5d5ff6476601541
Nick <87390744+visloq@users.noreply.github.com>
2021-09-03 13:23:05 -0500

    Updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <>
       <footer className="footerstyle w-full px-6 border-t">
         <div className="container mx-auto max-w-8xl py-6 flex flex-wrap md:flex-no-wrap justify-between items-center text-sm">
-          &copy;{new Date().getFullYear()} Minting Music. All rights
+          &copy;{new Date().getFullYear()} Seven Dead Stars. All rights
           reserved.
           <div className="pt-4 md:p-0 text-center md:text-right text-xs">
             <a href="#" className="text-black no-underline hover:underline">
