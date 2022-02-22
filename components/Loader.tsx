500af1615a19123e15dcd7d31a939529de672144
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-22 21:21:33 +0100

    created loader

---
+++ components/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return (
+        <div className="flex justify-center items-center py-3 mt-8">
+            <div className="animate-spin rounded-full mt-8 h-32 w-32 border-b-2 border-[#3b3b3b]" />
+        </div>
+    )
+}
+
+export default Loader;
