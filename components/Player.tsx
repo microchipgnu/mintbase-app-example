b5455f2c0a49724bfb45249bafa24fda9d0745b6
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-22 09:48:01 +0100

    started custom video player

---
+++ components/Player.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+function Player({src}: {src: string}) {
+    const [playIcon, setPlayIcon] = useState<string>('play');
+    const [volumeIcon, setVolumeIcon] = useState<string>('sound');
+
+
+    const playPause = (e: any) => {
+        e.stopPropagation()
+        playIcon == 'play' ?  setPlayIcon('pause') : setPlayIcon('play')   
+    }
+
+    const muteUnmute = (e: any) => {
+        e.stopPropagation()
+        volumeIcon == 'sound' ?  setVolumeIcon('mute') : setVolumeIcon('sound')   
+    }
+
+    return ( 
+        <div className="">
+            <div className="">
+            <video id="video">
+                <source src={src}/>
+            </video>
+            </div>
+            <div>
+                <button onClick={playPause}>
+                    { playIcon === "play" ? (
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    ) : (
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 9v6m4-6v6m7-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                    )
+                    }
+                </button>
+
+                <button  onClick={muteUnmute}>
+                    {
+                        volumeIcon === "sound" ? (
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" />
+                            </svg>
+                        ) : (
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clipRule="evenodd" />
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2" />
+                            </svg>
+                        )
+                    }
+                </button>
+            </div>
+        </div>
+     );
+}
+
+export default Player;
