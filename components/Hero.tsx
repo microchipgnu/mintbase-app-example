472d75559bf112e30aa03f4a5b97cceecbc94f95
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-22 20:55:55 +0100

    addde text gradient

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,26 +1,19 @@
-import Navbar from '../components/Navbar'
-
-import { useWallet } from '../services/providers/MintbaseWalletContext'
 import client from '../public/data/client.json'
-import Link from 'next/link'
-import Image from 'next/image'
-
 const Hero = () => {
-  const { wallet, isConnected, details } = useWallet()
   return (
     <>
       <div
-        className="w-full py-24 px-6 bg-cover bg-no-repeat bg-top relative z-10"
+        className="w-full py-24 px-6 bg-cover bg-no-repeat bg-top relative z-10 object-contain"
         style={{
           backgroundImage:
             "url('images/coverArt.jpg')",
         }}
       >
         <div className="container max-w-4xl mx-auto text-center">
-          <h1 className="fontFamily heroH1">
+          <h1 className="fontFamily text-gradient heroH1">
             {client.HeroTitle}
           </h1>
-          <p className="fontFamily heroP">
+          <p className="fontFamily heroP text-gradient">
             {client.HeroQuote}
           </p>
 
