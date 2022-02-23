dafb775d8514331f3527ccfd7e45fe1d8e709f30
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-23 22:51:53 +0100

    added gap

--- components/Products.tsx
+++ components/Products.tsx
@@ -69,7 +69,7 @@ const Products = ({ storeId }: { storeId: string }) => {
     <>
       {loadingTokensData && <Loader />}
       <div className="pt-6 pb-24 w-full mx-auto">
-        <div className="grid sm:grid-cols-2  md:grid-cols-3">
+        <div className="grid sm:grid-cols-2 gap-1 md:grid-cols-3">
           {metaData.map((meta: ProductMeta) => (
             <NFT
               key={meta.thing.id}
