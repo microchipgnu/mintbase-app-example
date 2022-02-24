16cc43f0d20a1f25ec354896833efbef987f1241
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-24 18:27:19 +0100

    changed background

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,17 +3,17 @@ const Hero = () => {
   return (
     <>
       <div
-        className="w-full py-24 px-6 bg-cover bg-no-repeat bg-top relative z-10 object-contain"
+        className="w-full py-24 px-6 bg-cover bg-no-repeat bg-center h-full relative z-10 object-contain mt-20 md:mt-24"
         style={{
           backgroundImage:
             "url('images/coverArt.jpg')",
         }}
       >
-        <div className="container max-w-4xl mx-auto text-center">
+        <div className="container max-w-4xl mx-auto my-auto">
           <h1 className="fontFamily text-gradient heroH1">
             {client.HeroTitle}
           </h1>
-          <p className="fontFamily heroP text-gradient">
+          <p className="fontFamily heroP text-gradien">
             {client.HeroQuote}
           </p>
 
