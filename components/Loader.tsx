5750e5841bf6a3aa829817bf768465e4f95f94fc
MZ Marshall <mzmarshall1278@gmail.com>
2022-02-24 21:54:01 +0100

    fixed loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,7 +1,7 @@
 const Loader = () => {
     return (
-        <div className="flex justify-center items-center py-3 mt-8">
-            <div className="animate-spin rounded-full mt-8 h-32 w-32 border-b-2 border-[#3b3b3b]" />
+        <div className="flex justify-center items-center py-3 mt-16 w-full">
+            <div className="animate-spin rounded-full mt-8 h-32 w-32 border-b-2 border-[#000e4f]" />
         </div>
     )
 }
